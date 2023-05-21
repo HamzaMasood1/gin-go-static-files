65eb23dfc414478e15e443f41196af5bf56c8296
HamzaMasood1 <hamzamasood183@gmail.com>
2023-05-21 19:27:26 +0100

    udpating recipe struct and loading json

--- main.go
+++ main.go
@@ -1,35 +1,39 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Recipe struct {
-	Name    string
-	Picture string
+	Name        string       `json:"name"`
+	Ingredients []Ingredient `json:"ingredients"`
+	Steps       []string     `json:"steps"`
+	Picture     string       `json:"imageURL"`
+}
+type Ingredient struct {
+	Quantity string `json:"quantity"`
+	Name     string `json:"name"`
+	Type     string `json:"type"`
+}
+
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
 }
 
 func IndexHandler(c *gin.Context) {
-	recipes := make([]Recipe, 0)
-	recipes = append(recipes, Recipe{
-		Name:    "Burger",
-		Picture: "/assets/images/burger.jpg",
-	})
-	recipes = append(recipes, Recipe{
-		Name:    "Pizza",
-		Picture: "/assets/images/pizza.jpg",
-	})
-	recipes = append(recipes, Recipe{
-		Name:    "Tacos",
-		Picture: "/assets/images/tacos.jpg",
-	})
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"recipes": recipes,
 	})
-	// c.File("index.html")
 }
+
 func main() {
 	router := gin.Default()
 	router.Static("/assets", "./assets")
